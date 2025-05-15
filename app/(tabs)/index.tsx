080471894cefeb31066a13ba121c30b72ded874a
Jonny Saykosy <josayko@pm.me>
2025-05-16 01:12:22 +0200

    feat: listening to clerk webhooks event in convex when user created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-import { Link } from 'expo-router';
-import { View } from 'react-native';
+import { styles } from '@/styles/auth.styles';
+import { useAuth } from '@clerk/clerk-expo';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const { signOut } = useAuth();
   return (
-    <View >
-      <Link href="/notifications">Feed screen in tabs</Link>
+    <View style={styles.container} >
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text style={{ color: 'white' }}>Signout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ convex/users.ts
@@ -0,0 +1,35 @@
+import { v } from 'convex/values';
+import { mutation } from './_generated/server';
+
+export const createUser = mutation({
+  args: {
+    username: v.string(),
+    fullname: v.string(),
+    image: v.string(),
+    bio: v.optional(v.string()),
+    email: v.string(),
+    clerKId: v.string(),
+  },
+
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query('users')
+      .withIndex('by_clerk_id', (q) => q.eq('clerKId', args.clerKId))
+      .first();
+
+    if (existingUser) return;
+
+    // create a user in db
+    await ctx.db.insert('users', {
+      username: args.username,
+      fullname: args.fullname,
+      image: args.image,
+      bio: args.bio,
+      email: args.email,
+      clerKId: args.clerKId,
+      followers: 0,
+      following: 0,
+      posts: 0,
+    });
+  },
+});
